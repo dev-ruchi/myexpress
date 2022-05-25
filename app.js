c31e17539d3198ad7a71396bd86dff88576f90be
Ruchi TechSter <ruchi.thanabhawan@gmail.com>
2022-05-25 18:43:00 +0530

    implement products CRUD

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
+require('dotenv').config()
 
 const app = express()
 
+app.use(express.json())
+
+const Product = require('./models/Products')
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(async () => {
+        console.log('connected to database');
+
+    }).catch(e => console.log(e))
+
+
 app.get('/products', async (req, res) => {
+    const products = await Product.find()
+    res.json(products);
 
-    const products = [
-        {
-            id: 1,
-            name: 'Iphoone-13',
-            price: 100000
-        },
-        {
-            id: 2,
-            name: 'Mixer',
-            price: 4000
-        }
-    ];
+})
 
-    return res.json(products);
+app.post('/products', async (req, res) => {
+    Product.create({
+        name: "T-shirt",
+        price: 500
+    })
+    console.log(req.body);
+    res.json(req.body)
+})
+
+app.delete('/products/:id', async ( req, res) => {
+    const product = await Product.deleteOne({ name: 'Something awesome' })
+    res.json(product);
+})
+
+app.put('/products/:id', async (req, res) => {
+    const product = await Product.findByIdAndUpdate("6289079b76f13dcc00b778ab", {price: 20000})
+    res.json(product);
 })
 
 
